276b255470721a998eb054025b2a6ba978b80844
Matej Malesevic <matej.malesevic@approppo.ch>
2022-06-12 23:06:15 +0200

    edited search teaserview

--- MyLib/Views/Search/SearchResultView.swift
+++ MyLib/Views/Search/SearchResultView.swift
@@ -14,22 +14,28 @@ struct SearchResultView: View {
         VStack (alignment:.leading) {
             HStack(alignment: .top) {
                 
-                ZStack{
-                    Rectangle()
-                        .foregroundColor(.Secondary)
-                        .cornerRadius(25, corners: [.topLeft, .bottomRight])
-                    if let url = searchResult.volumeInfo?.imageLinks?.thumbnail {
-                        AsyncImage(url: url)
-                    } else {
-                        Image(systemName: "photo.fill")
+                if let url = searchResult.volumeInfo?.imageLinks?.thumbnail {
+                    AsyncImage(url: url)
+                        .frame(width: 100, height: 150)
+                        .aspectRatio(2/3, contentMode: .fill)
+                        .cornerRadius(35, corners: [.topLeft, .bottomRight])
+                        .padding()
+                } else {
+                    ZStack{
+                        Rectangle()
+                            .foregroundColor(.Secondary)
+                            .cornerRadius(25, corners: [.topLeft, .bottomRight])
+                        Image(systemName: "book.circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80)
                             .foregroundColor(.Primary)
-                            .font(.title)
-                    }
-                    
-                }.frame(width: 80)
-                    .aspectRatio(4/3, contentMode: .fit)
-                    .padding()
-                
+                            
+                        
+                    }.frame(width: 100, height: 150)
+                        .aspectRatio(2/3, contentMode: .fill)
+                        .padding()
+                }
                 
                 VStack (alignment:.leading) {
                     Text(searchResult.volumeInfo?.title ?? "-")
@@ -39,10 +45,11 @@ struct SearchResultView: View {
                         Text(subtitle)
                             .font(.subheadline  )
                     }
-                    //                    if let year = searchResult.volumeInfo?.publishedDate, let authors = searchResult.volumeInfo?.authors?.joined(separator: ", ") {
-                    //                        Text("\(DateFormatter.yearFormatter.string(from: year)) - \(authors)")
-                    //                            .font(.body)
-                    //                    }
+                    if let publishedDate = searchResult.volumeInfo?.publishedDate, let authors = searchResult.volumeInfo?.authors?.joined(separator: ", ") {
+                        Text("\(publishedDate) - \(authors)")
+                            .font(.body)
+                            .padding(.vertical)
+                    }
                 }.padding([.top], 10)
             }.padding(.trailing)
             
@@ -73,10 +80,30 @@ struct SearchResultView: View {
     }
 }
 
+
 struct SearchResultView_Previews: PreviewProvider {
+    
+    static func previewBook(name: String) -> VolumeApiModel {
+        do {
+            let result: VolumeApiModel.APIResult = try JSONDecoder().loadObjectFromFile(fileName: name)
+            
+            guard let resultItem = result.items?.first else {
+                throw ApiError.noResult
+            }
+            
+            return resultItem
+            
+        } catch {
+            return VolumeApiModel(selfLink: "", volumeInfo: VolumeInfoApiModel(title: "Mastering Bitcoin", subtitle: "Unlocking Digital Cryptocurrencies", authors: ["Andreas M. Antonopoulos"], publisher: "O'Reilly Media", publishedDate: "2022", industryIdentifiers: [IndustryIdentifiersApiModel(type: "ISBN_13", identifier: "978-1-4919-5438-6")], pageCount: 302, language: "de"), kind: "book#softcover", id: UUID().description, etag: nil, searchInfo: SearchInfo(textSnippet: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."))
+        }
+    }
+    
     static var previews: some View {
-        let searchResult = VolumeApiModel(selfLink: "", volumeInfo: VolumeInfoApiModel(title: "Mastering Bitcoin", subtitle: "Unlocking Digital Cryptocurrencies", authors: ["Andreas M. Antonopoulos"], publisher: "O'Reilly Media", publishedDate: "2022", industryIdentifiers: [IndustryIdentifiersApiModel(type: "ISBN_13", identifier: "978-1-4919-5438-6")], pageCount: 302, language: "de"), kind: "book#softcover", id: UUID().description, etag: nil, searchInfo: SearchInfo(textSnippet: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."))
-        SearchResultView(searchResult: searchResult)
+        
+        Group{
+            SearchResultView(searchResult: previewBook(name: "JRR_Tolkien_The_Hobbit"))
+            SearchResultView(searchResult: previewBook(name: ""))
+        }
             .preferredColorScheme(.dark)
             .previewLayout(.sizeThatFits)
     }
